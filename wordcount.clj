3a8637f724204a40e6f66d802c86548e530e40d0
Tim <tim@libens.net>
2010-11-11 02:02:29 -0800

    added types to help people avoid spectacular performance degradation; removed one example from file; cleaned up test-reducer

--- wordcount.clj
+++ wordcount.clj
@@ -4,22 +4,14 @@
     (:import java.util.StringTokenizer))
 
 ;; Given an input line, return a list of key/value pairs.
-(defn mapper [v]
+(defn mapper [^String v]
   (let [words (enumeration-seq (StringTokenizer. v))]
     (map #(do [ % "1" ] ) words)
 ))
 
 ;; Given a key and a list of all values associated with that key, return the reduced value.
-(defn reducer [k v]
+(defn reducer [^String k ^String v]
   [ k (reduce + (map #(Integer/parseInt %) v)) ])
-
-    (defn mapper [v]
-        (let [[timehit userid] (.split v ",")]
-            [ [ userid timehit ] ]
-    ))
-
-    (defn reducer [k v]
-        [ k (min v) ])
 
 ;; 
 ;; Handy debugging functions
@@ -30,8 +22,7 @@
 
 (defn test-reducer [fname]
   (let [m (test-mapper fname)
-        g (merge (sorted-map) (group-by #(first %) m))
-        values (fn [v] (map #(nth % 1) v))]
-    (map (fn [[k v]] (reducer k (values v))) g)
+        g (merge (sorted-map) (group-by #(first %) m))]
+    (for [[k v] g]
+      (reducer k (map fnext v)))
 ))
-
